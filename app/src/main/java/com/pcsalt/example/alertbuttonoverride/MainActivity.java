a53ee52a8f191d3a8fa4a029b83eaa363d01e200
PCSalt <pcsalt@Rajneeshs-MacBook-Pro.local>
2016-05-21 21:44:27 +0530

    imported project into Android Studio

---
+++ app/src/main/java/com/pcsalt/example/alertbuttonoverride/MainActivity.java
@@ -0,0 +1,64 @@
+package com.pcsalt.example.alertbuttonoverride;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*
+    This method is referenced from Button in activity_main.xml
+     */
+    public void buttonClicked(View view) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Demo");
+        dialogBuilder.setMessage("After clicking the \"Done\" button, dialog will not close.");
+        dialogBuilder.setPositiveButton("Done", null);
+        dialogBuilder.setNegativeButton("Close", null);
+
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        // override the text color of positive button
+        positiveButton.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
+        // provides custom implementation to positive button click
+        positiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPositiveButtonClicked(alertDialog);
+            }
+        });
+
+        Button negativeButton = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+        // override the text color of negative button
+        negativeButton.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+        // provides custom implementation to negative button click
+        negativeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNegativeButtonClicked(alertDialog);
+            }
+        });
+
+    }
+
+    private void onPositiveButtonClicked(AlertDialog alertDialog) {
+        Toast.makeText(MainActivity.this, "Dialog will NOT close", Toast.LENGTH_SHORT).show();
+    }
+
+    private void onNegativeButtonClicked(AlertDialog alertDialog) {
+        Toast.makeText(MainActivity.this, "Dialog will close now", Toast.LENGTH_SHORT).show();
+        alertDialog.dismiss();
+    }
+}
